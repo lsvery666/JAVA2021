32dfc27cbbf7af953369f94b62f274a89072d0fc
lisen <466367105@qq.com>
2021-02-25 15:52:51 +0800

    First commit

---
+++ Collection/src/CollectionTest01.java
@@ -0,0 +1,17 @@
+/*
+    psvm    public static void main(String[] args){}
+    sout    System.out.println();
+    ctrl y  删除一行
+    左侧窗口列表展开关闭  左箭头关闭 右箭头展开 上下箭头移动
+    IDEA 中退出任何窗口都可以使用ESC键
+    任何新增/新建/添加的快捷键 alt + insert
+    窗口变大变小 ctrl shift F12
+
+ */
+public class CollectionTest01 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
